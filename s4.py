e9e57609c98804419c5e9749b008cfddffc55b9c
ritikadkdi <darkbhai1bhai@gmail.com>
2025-02-20 01:56:03 +0530

    Update s4.py

--- s4.py
+++ s4.py
@@ -12,7 +12,7 @@ import os
 # @RitikXyz099 GRP# insert your Telegram bot token here
 
     
-bot = telebot.TeleBot('7206131737:AAEmHOZePn592Mvk331I8VDD_nG7bj2AOr8')
+bot = telebot.TeleBot('7594583741:AAH3zWrkZwNPq2M3pUODI6c2VmvYI_M5fXQ')
 
 # @RitikXyz099 GRP # Admin user IDs
 admin_id = ["6437994839"]
